5fd75a2a0119d90c3e5ff7ffaf7760f98747a45d
pascalamah <pascal.amah10@gmail.com>
2025-04-30 19:48:37 +0100

    Task completed

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+0-minoperations
+Calculates the fewest number of operations needed to result in
+exactly n H characters using Copy All and Paste.
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    ops = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            ops += factor
+            n //= factor
+        factor += 1
+
+    return ops
